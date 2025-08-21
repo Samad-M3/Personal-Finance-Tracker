33b049343cb9052692a73858623c8aa9f27b237e
Samad <abdus-samad_mohit@hotmail.com>
2025-08-21 01:00:17 +0100

    Filtered through the transactions

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import csv
 import pandas as pd
 from pathlib import Path
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 transactions = []
 
 add_transaction = True
 
 start_adding_transactions = input("Would you like to start entering transactions (yes/no): ")
+
 if start_adding_transactions.lower() == "yes":
     while add_transaction:
         print("")
@@ -54,4 +56,27 @@ with open("transactions.csv", "a", newline="") as f:
     writer.writerows(transactions)
 
 df = pd.read_csv("transactions.csv")
-print(df.to_string())
+# print(df.to_string())
+
+""" Hard-coded filtering """
+
+''' Filter by Category '''
+
+# print(df[df["Category"] == "Food"]) 
+# print(df[df["Category"] == "Entertainment"])
+# print(df[df["Category"] == "Transport"])
+
+''' Filter by Month '''
+
+# print(df[df["Date"].str.contains("-09-")]) 
+# print(df[df["Date"].str.contains("-10-")])
+# print(df[df["Date"].str.contains("-12-")])
+
+''' Combined Filters '''
+
+# print(df[(df["Category"] == "Food") & (df["Date"].str.contains("-11-"))])
+# print(df[(df["Category"] == "Entertainment") & (df["Date"].str.contains("-08-"))])
+
+''' Aggregation '''
+
+# print(df[df["Date"].str.contains("-10-")].groupby("Category")["Amount"].sum()) # Total spending per category in October
