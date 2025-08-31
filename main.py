d7e7124990dbceb012596512e301be9b86f2e9f3
Samad <abdus-samad_mohit@hotmail.com>
2025-08-31 17:22:37 +0100

    Added docstrings and inline comments

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+"""
+Personal Finance Tracker CLI application.
+
+Allows users to add transactions, view summaries by month/category, 
+visualise summaries by generating charts using pandas and matplotlib.
+"""
+
 import pandas as pd
 from pathlib import Path
 import matplotlib.pyplot as plt
@@ -7,6 +14,13 @@ import sys
 import calendar
 
 class FinanceTracker:
+    """
+    A personal finance tracker that manages transactions and provides insights.
+
+    Stores income and expenses, and allows filtering, summarising, and 
+    visualising spending patterns.
+    """
+
     MONTHS_ABBR = {1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr", 5: "May", 6: "Jun", 7: "Jul", 8: "Aug", 9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec"}
     MONTHS_FULL = list(calendar.month_name)[1:]
     HEADERS = ["Date", "Category", "Amount", "Description"]
@@ -18,8 +32,16 @@ class FinanceTracker:
             EXPENSE_CATEGORIES.append(c)
     
     def __init__(self):
+        """
+        Initialises the DataFrame used to store transactions. 
+
+        Parameters
+        ----------
+        None
+        """
+
         if Path("transactions.csv").exists():
-            df = pd.read_csv("transactions.csv") # Reading from CSV file  
+            df = pd.read_csv("transactions.csv") 
             df["Date"] = pd.to_datetime(df["Date"], format="%d-%m-%Y") # Converting the dates column from strings to datetime objects to help with filtering transactions
         else:
             df = pd.DataFrame(columns = FinanceTracker.HEADERS)
@@ -27,6 +49,24 @@ class FinanceTracker:
         self.df = df
 
     def menu(self):
+        """
+        Displays the menu to the user.
+
+        Parameters
+        ----------
+        None
+
+        Raises
+        ------
+        ValueError 
+            If they enter an invalid option or select an option that is not
+            within the range of accepted values.
+
+        Returns
+        -------
+        None
+        """
+
         while True:
             while True:
                 try:
@@ -46,6 +86,31 @@ class FinanceTracker:
                 self.exit_program()
 
     def add_transaction(self):
+        """
+        Add one or more transactions to the tracker. 
+
+        The method prompts the user for details of their transaction, 
+        validates their input, appends it to the DataFrame, once the user
+        wishes to add no further transactions, transactions are saved to 
+        the CSV.
+
+        Parameters
+        ----------
+        None
+
+        Raises
+        ------
+        ValueError
+            If the date is invalid, category is not recognised,
+            amount does not match category type, or description is blank.
+
+            If option to add another transaction is not strictly "Yes" or "No".
+
+        Returns
+        -------
+        None
+        """
+
         total_transactions = 0
 
         while True:
@@ -56,7 +121,7 @@ class FinanceTracker:
             while True:
                 try:
                     date = input("Enter the date (DD-MM-YYYY): ")
-                    parsed_date = datetime.strptime(date, "%d-%m-%Y")
+                    parsed_date = datetime.strptime(date, "%d-%m-%Y") # Ensure the date entered is in the correct format
                 except ValueError as e:
                     print(e)
                 else:
@@ -77,6 +142,7 @@ class FinanceTracker:
             while True:
                 try:
                     amount = float(input("Enter the amount (positive for income/refund, negative for expenses): "))
+                    # Ensure sign matches category: income/refunds must be positive, expenses must be negative
                     if category in FinanceTracker.INCOME_CATEGORIES and amount <= 0:
                         raise ValueError(f"{category} must be positive")
                     elif category not in FinanceTracker.INCOME_CATEGORIES and amount >= 0:
@@ -102,7 +168,8 @@ class FinanceTracker:
                 "Amount": amount,
                 "Description":description
             }
-            self.df = pd.concat([self.df, pd.DataFrame([transaction])], ignore_index = True)
+            # Append the new transaction (as a 1-row DataFrame) to the existing DataFrame, and reset the index to keep it continuous
+            self.df = pd.concat([self.df, pd.DataFrame([transaction])], ignore_index = True) 
 
             print("")
 
@@ -125,6 +192,24 @@ class FinanceTracker:
                 break
 
     def sub_menu(self):
+        """
+        Displays the sub-menu to the user.
+
+        Parameters
+        ----------
+        None
+
+        Raises
+        ------
+        ValueError 
+            If they enter an invalid option or select an option that is not
+            within the range of accepted values.
+
+        Returns
+        -------
+        None
+        """
+
         while True:
             while True:
                 try:
@@ -153,10 +238,47 @@ class FinanceTracker:
     
     @staticmethod
     def exit_program():
+        """
+        Helper method to terminate the program safely. 
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
+
         print("Closing program...")
         sys.exit(0)
 
     def view_by_month(self):
+        """
+        Displays a financial summary for a given month.
+
+        Prompts the user to enter a month. Calculates the sum for each category
+        and calculates total income/expense. 
+        
+        Optionally displays:
+            - Bar chart of expenses by category
+            - Horizontal bar chart of income vs expenses
+            - Pie chart of expense breakdown
+
+        Parameters
+        ----------
+        None
+
+        Raises
+        ------
+        ValueError
+            If the month is not a valid month name.
+
+        Returns
+        -------
+        None
+        """
+
         while True:
             try: 
                 month = input(f"\nEnter the full month name: ").strip().capitalize()
@@ -171,12 +293,16 @@ class FinanceTracker:
 
         print(f"\nSummary for {month}:\n")
 
-        summary = self.df[self.df["Date"].dt.month_name() == month].groupby("Category")["Amount"].sum()
-        summary.index.name = None
-        total_income = self.df[(self.df["Date"].dt.month_name() == month) & (self.df["Category"].isin(FinanceTracker.INCOME_CATEGORIES))]["Amount"].sum()
+        # Filter rows for the given month, then group by category and sum the amounts
+        summary = self.df[self.df["Date"].dt.month_name() == month].groupby("Category")["Amount"].sum()  
+
+        # Filter rows for the given month and categories in INCOME_CATEGORIES, then sum the amounts
+        total_income = self.df[(self.df["Date"].dt.month_name() == month) & (self.df["Category"].isin(FinanceTracker.INCOME_CATEGORIES))]["Amount"].sum() 
+
+        # Filter rows for the given month and categories in EXPENSE_CATEGORIES, then sum the amounts
         total_expenses = self.df[(self.df["Date"].dt.month_name() == month) & (self.df["Category"].isin(FinanceTracker.EXPENSE_CATEGORIES))]["Amount"].sum()
 
-        for category, amount in summary.items():
+        for category, amount in summary.items(): 
             print(f"{category}: £{amount:.2f}")
 
         print(f"\nTotal income: £{total_income:.2f} \nTotal expenses: £{total_expenses:.2f}")
@@ -184,14 +310,15 @@ class FinanceTracker:
         print(f"\n----------------------------")
 
         if self.get_visualisation_choice() == "Yes":
-            summary_without_income = summary.drop(FinanceTracker.INCOME_CATEGORIES, errors = "ignore")
+            # Drops any categories listed in INCOME_CATEGORIES if they exist, if they don't, no error is raised
+            summary_without_income = summary.drop(FinanceTracker.INCOME_CATEGORIES, errors = "ignore") 
 
             fig = plt.figure(figsize = (8, 5))
             fig.canvas.manager.set_window_title(f"Month: {month}")
 
             ''' Chart 1 '''
             x1 = np.array(summary_without_income.index)
-            y1 = np.array(summary_without_income.values.__abs__())
+            y1 = np.array(summary_without_income.values.__abs__()) # Convert expenses to absolute values so bars display as positive amounts
 
             plt.subplot(3, 1, 1)
             plt.grid(alpha = 0.3, linestyle = '--')
@@ -225,6 +352,32 @@ class FinanceTracker:
             plt.show()
 
     def view_by_category(self):
+        """
+        Displays a financial summary for a given category.
+
+        Prompts the user to enter a category. Calculates the sum for each month in the year.
+        Additionally, it calculates the number of transactions for each month for that category and also 
+        calculates the average spent per transaction for each month. 
+
+        Optionally displays:
+            - Bar chart of category by month
+            - Line chart of average per transaction by month
+            - Bar chart of total number of transactions by month
+
+        Parameters
+        ----------
+        None
+
+        Raises
+        ------
+        ValueError
+            If the category is not a valid category.
+
+        Returns
+        -------
+        None
+        """
+
         while True:
             try:
                 print("\nCategories:\n")
@@ -241,8 +394,10 @@ class FinanceTracker:
 
         print(f"\nSummary for {category} category:\n")
 
+        # Filter rows for the given category, then group by month and sum the amounts 
         summary = self.df[self.df["Category"] == category].groupby(self.df["Date"].dt.month)["Amount"].sum()
-        summary.index.name = None
+        
+        # Count the number of transactions per month for the given category
         number_of_transactions = self.df[self.df["Category"] == category].groupby(self.df["Date"].dt.month)["Amount"].count()
         amount_of_transactions_per_month = []
 
@@ -274,7 +429,7 @@ class FinanceTracker:
 
             ''' Chart 1'''
             x1 = np.array(month_names)
-            y1 = np.array(summary.values.__abs__())
+            y1 = np.array(summary.values.__abs__()) # Convert the values to absolute values so bars display as positive amounts
 
             plt.subplot(3, 1, 1)
             plt.grid(alpha = 0.3, linestyle = '--')
@@ -290,7 +445,7 @@ class FinanceTracker:
 
             plt.subplot(3, 1, 2)
             plt.grid(alpha = 0.3, linestyle = '--')
-            plt.plot(x2, y2, marker = "o", mfc = "#2ca02c", c = "#2ca02c", ls = "--", linewidth =  1, markersize = 4)
+            plt.plot(x2, y2, marker = "o", mfc = "#2ca02c", c = "#2ca02c", linestyle = "--", linewidth =  1, markersize = 4)
 
             plt.title(chart2_title)
             plt.xlabel("Months")
@@ -312,15 +467,37 @@ class FinanceTracker:
             plt.show()
 
     def view_cumulative_net_balance(self):
+        """
+        Displays the cumulative net balance for the year.
+
+        Calculates the net balance (total income - total expense) for each month
+        and then works out the cumulative net balance.
+
+        Optionally displays:
+            - Bar chart of net balance by month 
+            - Line chart of cumulative net balance by month
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
+
         print(f"\n----------------------------")
 
-        print(f"\nCumulative net balance:\n")
+        print(f"\nCumulative Net Balance:\n")
 
+        # Filter rows for categories in INCOME_CATEGORIES, then group by month and sum the amounts
         total_income_per_month = self.df[self.df["Category"].isin(FinanceTracker.INCOME_CATEGORIES)].groupby(self.df["Date"].dt.month)["Amount"].sum()
+
+        # Filter rows for categories in EXPENSE_CATEGORIES, then group by month and sum the amounts 
         total_expenses_per_month = self.df[self.df["Category"].isin(FinanceTracker.EXPENSE_CATEGORIES)].groupby(self.df["Date"].dt.month)["Amount"].sum()
+
         net = total_income_per_month + total_expenses_per_month
-        cumulative_net_balance = net.cumsum()
-        net.index.name = None
+        cumulative_net_balance = net.cumsum() # Work out the cumulative net balance over time
 
         for month, amount in cumulative_net_balance.items():
             print(f"{FinanceTracker.MONTHS_ABBR[month]}: £{amount:.2f}")
@@ -361,14 +538,35 @@ class FinanceTracker:
             plt.show()
 
     def view_all_time_overview(self):
+        """
+        Displays an all-time overview of the balance from start to end of the year.
+
+        Calcualtes the sum for each category and calculates the total income/expenses.
+
+        Optionally displays:
+            - Bar chart of expenses by category
+            - Horizontal bar chart of income vs expenses
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
+
         print(f"\n----------------------------")
 
-        print(f"\nAll-time overview per category:\n")
+        print(f"\nAll-time Overview by Category:\n")
 
+        # Groups by category and sums the amounts
         summary = self.df.groupby("Category")["Amount"].sum()
-        summary.index.name = None
 
+        # Filters rows for categories in INCOME_CATEGORIES, then sums the amounts 
         total_income = self.df[self.df["Category"].isin(FinanceTracker.INCOME_CATEGORIES)]["Amount"].sum()
+
+        # Filters rows for categories in EXPENSE_CATEGORIES, then sums the amounts 
         total_expenses = self.df[self.df["Category"].isin(FinanceTracker.EXPENSE_CATEGORIES)]["Amount"].sum()
 
         for category, amount in summary.items():
@@ -379,6 +577,7 @@ class FinanceTracker:
         print(f"\n----------------------------")
 
         if self.get_visualisation_choice() == "Yes":
+            # Drops any categories listed in INCOME_CATEGORIES if they exist, if they don't, no error is raised
             summary_without_income = summary.drop(FinanceTracker.INCOME_CATEGORIES, errors = "ignore")
             
             fig = plt.figure(figsize = (8, 5))
@@ -386,7 +585,7 @@ class FinanceTracker:
 
             ''' Chart 1 '''
             x1 = np.array(summary_without_income.index)
-            y1 = np.array(summary_without_income.values.__abs__())
+            y1 = np.array(summary_without_income.values.__abs__()) # Convert expenses to absolute values so bars display as positive amounts
 
             plt.subplot(2, 1, 1)
             plt.grid(alpha = 0.3, linestyle = '--')
@@ -413,6 +612,24 @@ class FinanceTracker:
 
     @staticmethod
     def get_visualisation_choice():
+        """
+        Helper method to ask the user whether they want a visual representation of the data.
+
+        Parameters
+        ----------
+        None
+
+        Raises
+        ------
+        ValueError
+            If choice is not strictly "Yes" or "No".
+
+        Returns
+        -------
+        str
+            The user's choice ("Yes" or "No").
+        """
+
         while True:
             try:
                 choice = input(f"\nWould you like a visual representation of this data (Yes/No)? ").strip().capitalize()
@@ -424,10 +641,22 @@ class FinanceTracker:
                 return choice
             
     def save_to_csv(self):
-        # Save the whole df to CSV in one go
+        """
+        Saves the DataFrame to the CSV.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
+
+        # Save the whole DataFrame to CSV in one go
         df_to_save = self.df.copy()
-        df_to_save["Date"] = df_to_save["Date"].dt.strftime("%d-%m-%Y")
-        df_to_save.to_csv("transactions.csv", index = False)
+        df_to_save["Date"] = df_to_save["Date"].dt.strftime("%d-%m-%Y") # Format the Date column as DD-MM-YYYY strings
+        df_to_save.to_csv("transactions.csv", index = False) 
 
 ft = FinanceTracker()
 ft.menu()
